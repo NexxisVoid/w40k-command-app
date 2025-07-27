6c6824672221124b41228922c9da90c9bea06ad3
NexxisVoid <fgalella112@gmail.com>
2025-07-27 13:59:40 -0400

    Add files via upload

--- Main.tsx
+++ Main.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { registerSW } from 'virtual:pwa-register';
-
-registerSW({
-  onNeedRefresh() {
-    if (confirm("New version available. Refresh now?")) location.reload();
-  }
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
